45e9a4a757dcb04d82f227536947bb51b7c2b06a
Mikdan Jey <mikdanjey@gmail.com>
2023-02-03 01:17:24 +0530

    App Instlaled

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Python-Stripe
-Python Stripe
+# Accept a Payment
+
+Build a simple checkout form to collect payment details. Included are some basic
+build and run scripts you can use to start up the application.
+
+## Running the sample
+
+1. Build the server
+
+~~~
+pipenv install
+pipenv shell
+~~~
+
+2. Run the server
+
+~~~
+set FLASK_APP=server.py
+flask run --host=0.0.0.0 --port=5000
+~~~
+
+3. Go to [http://localhost:5000/checkout.html](http://localhost:5000/checkout.html)
---
+++ server.py
@@ -0,0 +1,43 @@
+import json
+import os
+import stripe
+
+# This is a public sample test API key.
+# Don’t submit any personally identifiable information in requests made with this key.
+# Sign in to see your own test API key embedded in code samples.
+stripe.api_key = os.environ.get("STRIPE_KEY")
+
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__, static_folder='public', static_url_path='', template_folder='public')
+
+def calculate_order_amount(items):
+    # Replace this constant with a calculation of the order's amount
+    # Calculate the order total on the server to prevent
+    # people from directly manipulating the amount on the client
+    return 5
+
+@app.route("/")
+def checkout():
+    return render_template('checkout.html')
+
+@app.route('/create-payment-intent', methods=['POST'])
+def create_payment():
+    try:
+        data = json.loads(request.data)
+        # Create a PaymentIntent with the order amount and currency
+        intent = stripe.PaymentIntent.create(
+            amount=calculate_order_amount(data['items']),
+            currency='usd',
+            automatic_payment_methods={
+                'enabled': True,
+            },
+        )
+        return jsonify({
+            'clientSecret': intent['client_secret']
+        })
+    except Exception as e:
+        return jsonify(error=str(e)), 403
+
+if __name__ == '__main__':
+    app.run(port=5000)
